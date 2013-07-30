2a8f2eed7c8154f49a75bc1c999ecc5bd12b68e7
Jezabel <alina.ks9403@gmail.com>
2013-07-30 11:38:53 -0600

    V7

--- src/server/scripts/Spells/spell_priest.cpp
+++ src/server/scripts/Spells/spell_priest.cpp
@@ -141,7 +141,7 @@ class spell_pri_glyph_of_prayer_of_healing : public SpellScriptLoader
         }
 };
 
-// -47788 - Guardian Spirit
+// 47788 - Guardian Spirit
 class spell_pri_guardian_spirit : public SpellScriptLoader
 {
     public:
@@ -215,7 +215,7 @@ class spell_pri_item_greater_heal_refund : public SpellScriptLoader
                 return true;
             }
 
-            void HandleProc(AuraEffect const* aurEff, ProcEventInfo& eventInfo)
+            void HandleProc(AuraEffect const* aurEff, ProcEventInfo& /*eventInfo*/)
             {
                 PreventDefaultAction();
                 GetTarget()->CastSpell(GetTarget(), SPELL_PRIEST_ITEM_EFFICIENCY, true, NULL, aurEff);
@@ -265,7 +265,7 @@ class spell_pri_lightwell_renew : public SpellScriptLoader
         }
 };
 
-// -8129 - Mana Burn
+// 8129 - Mana Burn
 class spell_pri_mana_burn : public SpellScriptLoader
 {
     public:
